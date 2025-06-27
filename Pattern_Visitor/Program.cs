bb1c9a244d4f7d7c97bdb36e91a5d00c1a704fb3
MainPC <ilya6joker@gmail.com>
2025-06-27 22:05:53 +1000

    Add project files.

---
+++ Pattern_Visitor/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Pattern_Visitor
+{
+    internal class Program
+    {
+        interface IVisitor
+        {
+            public void Visit(Zoo zoo);
+            public void Visit(Cinema cinema);
+            public void Visit(Circus circus);
+
+        }
+
+        interface IPlace
+        {
+
+        }
+        public static void Main(string[] args)
+        {
+
+        }
+    }
+}

e8f43f90355ee9feb30347a13d4a8cdb9703b88b
MainPC <ilya6joker@gmail.com>
2025-06-28 20:15:26 +1000

    !Done pattern AbstractFactory

--- AbstractFactory/Program.cs
+++ AbstractFactory/Program.cs
@@ -8,11 +8,79 @@
 
 namespace Pattern_AbstractFactory
 {
+    interface IEngine
+    {
+        void ReleaseEngine();
+    }
+
+    class JapaneeseEngine : IEngine
+    {
+        public void ReleaseEngine() => Console.WriteLine("Japaneese engine");
+    }
+
+    class RussianEngine : IEngine
+    {
+        public void ReleaseEngine() => Console.WriteLine("Russian engine");
+    }
+
+    interface ICar
+    {
+        void ReleaseCar(IEngine engine);
+    }
+
+    class JapaneeseCar : ICar
+    {
+        public void ReleaseCar(IEngine engine)
+        {
+            Console.Write("Release Japanees car: ");
+            engine.ReleaseEngine();
+        }
+    }
+
+    class RussianCar : ICar
+    {
+        public void ReleaseCar(IEngine engine)
+        {
+            Console.Write("Release Russian car: ");
+            engine.ReleaseEngine();
+        }
+    }
+
+    interface IFactory
+    {
+        IEngine createEngine();
+        ICar createCar();
+    }
+
+    class JapaneeseFactory : IFactory
+    {
+        public IEngine createEngine() => new JapaneeseEngine();
+        public ICar createCar() => new JapaneeseCar();
+
+    }
+
+    class RussianFactory : IFactory
+    {
+        public IEngine createEngine() => new RussianEngine();
+        public ICar createCar() => new RussianCar();
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var jFactory = new JapaneeseFactory();
+            var jEngine = jFactory.createEngine();
+            var jCar = new JapaneeseCar();
+            jCar.ReleaseCar(jEngine);
+
+            var rFactory = new RussianFactory();
+            var rEngine = rFactory.createEngine();
+            var rCar = new RussianCar();
+            rCar.ReleaseCar(rEngine);
+
+
+            Console.ReadKey();
         }
     }
 }

ff1b09d2e4522dcd0fdd99c8bfe88c5d64589d2d
MainPC <ilya6joker@gmail.com>
2025-06-29 11:58:04 +1000

    # Done Singleton pattern

---
+++ Singleton/Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+Пример порождающего шаблона проектирования программ «Одиночка»,
+который гарантирует, что в приложении будет создан единственный
+экземпляр некоторого класса и предоставляет глобальную точку доступа
+к этому экземпляру
+*/
+
+namespace Singleton
+{
+    public class DatabaseHelper
+    {
+        private string data;
+        private static DatabaseHelper databaseConnection;
+
+        private DatabaseHelper() => Console.WriteLine("Connection to Database");
+        public static DatabaseHelper GetConnection()
+        {
+            if (databaseConnection == null)
+                databaseConnection = new DatabaseHelper();
+            return databaseConnection;
+        }
+
+        public string SelectData() => data;
+        public void InsertData(string data)
+        {
+            Console.WriteLine($"New data: {data}, insert to Database" );
+            this.data = data;
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var connection = new DatabaseHelper();
+            DatabaseHelper.GetConnection().InsertData("12312");
+
+            Console.WriteLine($"Selected data: {DatabaseHelper.GetConnection().SelectData()}");
+
+            Console.ReadKey();
+        }
+    }
+}
